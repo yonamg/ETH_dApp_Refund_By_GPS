0e1ae13c279d13ad3e05e4f4c81e36b48e97230f
yonamg <softeng2006@gmail.com>
2022-10-26 22:22:50 +0300

    creatinf withdrawal function

--- contracts/lock.sol
+++ contracts/lock.sol
@@ -20,3 +20,16 @@ contract Lock {
         unlockTime = _unlockTime;
         owner = payable(msg.sender);
     }
+
+     function withdraw() public {
+        // Uncomment this line to print a log in your terminal
+        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+
+        require(block.timestamp >= unlockTime, "You can't withdraw yet");
+        require(msg.sender == owner, "You aren't the owner");
+
+        emit Withdrawal(address(this).balance, block.timestamp);
+
+        owner.transfer(address(this).balance);
+    }
+}
