a307e52d45b4534d67ba0e462212af572763ba3f
yonamg <softeng2006@gmail.com>
2022-10-26 22:38:54 +0300

    create employee details

---
+++ contracts/refunding.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.6.0 <0.9.0;
+
+
+//import "hardhat/console.sol";
+struct EmployeeDetail { 
+        string name; 
+        uint256 latitude;
+        uint256 longitude;
+        uint256 allowedDistance;
+        uint256 amount;
+    }
