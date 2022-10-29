c65e1e406c2b9613c2980f2e5431db16381e8762
yonamg <softeng2006@gmail.com>
2022-10-29 19:23:49 +0300

    import packages

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import './App.css';
+import React, { useState } from 'react';
+import { ethers } from 'ethers';
+
+
+import Lock from "./artifacts/contracts/refund.sol/Refund.json"
+
+const LockAddress = "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707"
