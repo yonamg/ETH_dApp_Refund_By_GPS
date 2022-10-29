b60744f0b8e2eeadd03fc3934a3b3b762ae3c443
yonamg <softeng2006@gmail.com>
2022-10-29 19:38:47 +0300

    transfer function

--- test/refund.js
+++ test/refund.js
@@ -107,4 +107,21 @@ const {
                     .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
                 });
               });
+
+              describe("Transfers", function () {
+                it("Should transfer the funds to the owner", async function () {
+                  const { lock, unlockTime, lockedAmount, owner } = await loadFixture(
+                    deployOneYearLockFixture
+                  );
+          
+                  await time.increaseTo(unlockTime);
+          
+                  await expect(lock.withdraw()).to.changeEtherBalances(
+                    [owner, lock],
+                    [lockedAmount, -lockedAmount]
+                  );
+                });
+              });
+            });
+          });
   
