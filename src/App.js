c8833243d8879cc022d05dbd76c3c2c6d984d7bd
yonamg <softeng2006@gmail.com>
2022-10-29 19:24:27 +0300

    add function app

--- src/App.js
+++ src/App.js
@@ -6,3 +6,14 @@ import { ethers } from 'ethers';
 import Lock from "./artifacts/contracts/refund.sol/Refund.json"
 
 const LockAddress = "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707"
+
+function App() {
+    const [inputList, setInputList] = useState([{ EmpName: "", publicAddress: "" }]);
+  
+    // handle input change
+    const handleInputChange = (e, index) => {
+      const { name, value } = e.target;
+      const list = [...inputList];
+      list[index][name] = value;
+      setInputList(list);
+    };
